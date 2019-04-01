02846ed10a894ae0f9e4d21b26d567f88851e446
DenicaRuseva <drr_87@abv.bg>
2019-04-01 09:52:19 +0300

    add shop component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import { Route } from 'react-router-dom';
+import Shop from './container/Shop/Shop';
 
 class App extends Component {
   render() {
     return (
       <Layout>
         <Route path="/" exact render={() => <div>carusel</div>} />
-        <Route path="/shop" exact render={() => <div>shop</div>}/>
+        <Route path="/shop" exact component={Shop}/>
       </Layout>
     );
   }
---
+++ src/container/Shop/Shop.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Shop extends Component {
+
+    render(){
+        return <div>shop</div>
+    };
+};
+
+export default Shop;
