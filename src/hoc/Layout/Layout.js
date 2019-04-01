351173e8d9ba71ff649994ec2e4accce593da756
DenicaRuseva <drr_87@abv.bg>
2019-04-01 06:11:22 +0300

    layout component

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+
+    render(){
+        return(
+            <div>
+                <div>header</div>
+                <div>carusel</div>
+                <div>shop</div>
+                <div>footer</div>
+            </div>
+        )
+    };
+};
+
+export default Layout;
